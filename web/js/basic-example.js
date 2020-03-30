3a12d9b8a33558b102d3fddac944e4e3f94c7963
Elke Steegmans <Elke.Steegmans@ucll.be>
2020-03-30 14:42:45 +0200

    Little things adapted ...

--- web/js/basic-example.js
+++ web/js/basic-example.js
@@ -1,1 +1,1 @@
-// looping$('li.hot').addClass('favorite');// chaining$('li:lt(3)').hide().fadeIn(1500);//event$('li').on('click', function() {  $(this).remove();});
+// looping$('li.hot').addClass('favorite');// chaining$('li:lt(3)').hide().fadeIn(1500);//event$('li').on('click', function() {  $(this).remove();});
